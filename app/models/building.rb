4f9162aa0586a05ef2410744fe9f5800859a98f4
sbal13 <sbal13@gmail.com>
2018-08-13 16:05:11 +0100

    added building helper functions

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,17 @@
 class Building < ApplicationRecord
+
+	def number_of_floors_available
+		# Will not work until relationships and schema are corretly setup
+
+		all_floors = Array(1..self.number_of_floors)
+		self.offices.each do |office|
+			all_floors.delete(office.floor)
+		end
+		all_floors
+	end
+
+	def empty_offices
+		number_of_floors_available.map { |f| offices.build(floor: f) }
+	end
+
 end
